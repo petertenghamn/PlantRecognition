58d7084ffe2d52d9a95a45f885f8982ca77d5cd7
Peter Tenghamn <petertenghamn@hotmail.com>
2021-05-08 18:50:30 +0200

    Obj Detector 1

---
+++ ObjDetectV01/ImagePrediction.cs
@@ -0,0 +1,9 @@
+﻿
+namespace ObjDetectV01
+{
+    class ImagePrediction : ImageData
+    {
+        public float[] Score { get; set; }
+        public string PredictedLabelValue { get; set; }
+    }
+}
